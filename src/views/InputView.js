c4f22064ccad19aa7e89d6b605f534b3ffbff9e4
Parksangwoo <psu8430@gmail.com>
2024-12-04 18:53:47 +0900

    driver: 상우, navigator: 수연

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,7 +1,21 @@
 import { Console } from '@woowacourse/mission-utils';
 
 const InputView = {
+  readMenuAndQuantity() {
+    try {
+      const userInput = Console.readLineAsync(
+        '주문하실 메뉴와 수량을 입력해주세요. ex) 피자(2개), 감자튀김(1개), 콜라(3개)',
+      );
 
-}
+      const menus = userInput.split(', ');
+
+      //Validator
+
+      return userInput;
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  },
+};
 
 export default InputView;
